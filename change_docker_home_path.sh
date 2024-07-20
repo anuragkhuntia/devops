1c8fcc774be490c3528477ef763ba087dae95ece
anuragkhuntia <kanurag03@gmail.com>
2024-07-20 16:10:06 +0530

    initial commit

---
+++ change_docker_home_path.sh
@@ -0,0 +1,1 @@
+echo '{"graph": "/app/docker","fixed-cidr": "","default-address-pools":[{"base":"169.254.0.0/16","size":24}]}' > /etc/docker/daemon.json
---
+++ extend_logical_volume.sh
@@ -0,0 +1,2 @@
+lvextend -L +10G /dev/vg_app/lv_app
+xfs_growfs /dev/vg_app/lv_app
